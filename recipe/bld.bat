257b42546cbc316d8ee467e1495fc118ff118b1a
Uwe L. Korn <uwe.korn@quantco.com>
2023-12-05 15:49:52 +0100

    Add int4.h

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,3 +5,6 @@ if errorlevel 1 exit 1
 
 cp ml_dtypes\include\float8.h %LIBRARY_PREFIX%\include\ml_dtypes\include\float8.h
 if errorlevel 1 exit 1
+
+cp ml_dtypes\include\int4.h %LIBRARY_PREFIX%\include\ml_dtypes\include\int4.h
+if errorlevel 1 exit 1
